67ef55a904cc6f83686594c2c776f2422db74827
Saad Abdul Majid <saad.majid@7vals.com>
2025-07-26 12:44:17 +0500

    Move `execute_command` to separate module and add a test

---
+++ itam_agent/src/command.rs
@@ -0,0 +1,37 @@
+use std::process::Command;
+
+pub fn execute_command(command: &str) -> Result<String, String> {
+    let result = Command::new("sh")
+        .arg("-c")
+        .arg(command)
+        .output();
+
+    match result {
+        Ok(output) => {
+            if output.status.success() {
+                Ok(String::from_utf8_lossy(&output.stdout).trim().to_string())
+            } else {
+                Err(format!(
+                    "{}:{}",
+                    output.status,
+                    String::from_utf8_lossy(&output.stderr).trim()
+                ))
+            }
+        }
+        Err(e) => Err(format!("command execution failed with: {}", e)),
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_execute_command() {
+        let result = execute_command("system_profiler SPHardwareDataType | grep 'Hardware UUID' | awk -F ':' {'print $2'}");
+        match result {
+            Ok(system_uuid) => assert_eq!(system_uuid, "B4569DDD-051D-5670-8150-AC13E67E38CF"),
+            Err(_) => panic!(""),
+        }
+    }
+}
--- itam_agent/src/main.rs
+++ itam_agent/src/main.rs
@@ -1,33 +1,11 @@
-use std::process::Command;
-
-fn execute_command(command: &str) -> Result<String, String> {
-    let result = Command::new("sh")
-        .arg("-c")
-        .arg(command)
-        .output();
-
-    match result {
-        Ok(output) => {
-            if output.status.success() {
-                Ok(String::from_utf8_lossy(&output.stdout).trim().to_string())
-            } else {
-                Err(format!(
-                    "{}:{}",
-                    output.status,
-                    String::from_utf8_lossy(&output.stderr).trim()
-                ))
-            }
-        }
-        Err(e) => Err(format!("command execution failed with: {}", e)),
-    }
-}
+mod command;
 
 fn main() {
     println!("starting with the name of Allah the most Merciful the most Benovelent");
 
     // system UUID
     println!("fetching system UUID");
-    match execute_command("system_profiler SPHardwareDataType | grep 'Hardware UUID' | awk -F ':' {'print $2'}") {
+    match command::execute_command("system_profiler SPHardwareDataType | grep 'Hardware UUID' | awk -F ':' {'print $2'}") {
         Ok(system_uuid) => {
             println!("system UUID: {}", system_uuid);
         }
@@ -38,7 +16,7 @@ fn main() {
 
     // system model
     println!("fetching system model");
-    match execute_command("system_profiler SPHardwareDataType | grep 'Model Identifier' | awk -F ':' {'print $2'}") {
+    match command::execute_command("system_profiler SPHardwareDataType | grep 'Model Identifier' | awk -F ':' {'print $2'}") {
         Ok(model_identifier) => {
             println!("system model: {}", model_identifier);
         }
@@ -49,7 +27,7 @@ fn main() {
 
     // system serial number
     println!("fetching system serial number");
-    match execute_command("system_profiler SPHardwareDataType | grep 'Serial Number' | awk -F ':' {'print $2'}") {
+    match command::execute_command("system_profiler SPHardwareDataType | grep 'Serial Number' | awk -F ':' {'print $2'}") {
         Ok(serial_number) => {
             println!("system serial number: {}", serial_number);
         }
@@ -60,7 +38,7 @@ fn main() {
 
     // system firmware version
     println!("fetching system firmware version");
-    match execute_command("system_profiler SPHardwareDataType | grep 'System Firmware Version' | awk -F ':' {'print $2'}") {
+    match command::execute_command("system_profiler SPHardwareDataType | grep 'System Firmware Version' | awk -F ':' {'print $2'}") {
         Ok(firmware_version) => {
             println!("system firmware version: {}", firmware_version);
         }
