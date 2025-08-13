38b956f055fcd6fb504ba0fdf4c3a741be02118b
Saad Abdul Majid <saad.majid@7vals.com>
2025-08-13 22:35:20 +0500

    Organised code into hardware and software modules

--- itam_agent/src/main.rs
+++ itam_agent/src/main.rs
@@ -1,5 +1,6 @@
 mod command;
-mod os;
+mod hardware;
+mod software;
 use log;
 use flexi_logger::Logger;
 
@@ -12,43 +13,43 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     // system UUID
     log::trace!("fetching system UUID");
-    match os::get_system_uuid() {
+    match hardware::bios::get_system_uuid() {
         Ok(system_uuid) => log::debug!("system UUID: {}", system_uuid.trim()),
         Err(e) => log::error!("failed to fetch system UUID: {}", e),
     }
 
     // system model
     log::trace!("fetching system model");
-    match os::get_system_model() {
+    match hardware::bios::get_system_model() {
         Ok(system_model) => log::debug!("system model: {}", system_model.trim()),
         Err(e) => log::error!("failed to fetch system model: {}", e),
     }
 
     // system serial number
     log::trace!("fetching system serial number");
-    match os::get_system_serial_number() {
+    match hardware::bios::get_system_serial_number() {
         Ok(system_serial_number) => log::debug!("system serial number: {}", system_serial_number.trim()),
         Err(e) => log::error!("failed to fetch system serial number: {}", e),
     }
 
     // system firmware version
     log::trace!("fetching system firmware version");
-    match os::get_system_firmware_version() {
+    match hardware::bios::get_system_firmware_version() {
         Ok(system_firmware_version) => log::debug!("system firmware version: {}", system_firmware_version.trim()),
         Err(e) => log::error!("failed to fetch system firmware version: {}", e),
     }
 
-    match os::get_os_name() {
+    match software::os::get_os_name() {
         Ok(os_name) => log::debug!("os name: {}", os_name),
         Err(e) => log::error!("failed to fetch os name: {}", e),
     }
 
-    match os::get_os_version() {
+    match software::os::get_os_version() {
         Ok(os_version) => log::debug!("os version: {}", os_version),
         Err(e) => log::error!("failed to fetch os version: {}", e),
     }
 
-    match os::get_os_comments() {
+    match software::os::get_os_comments() {
         Ok(os_comments) => log::debug!("os comments: {}", os_comments),
         Err(e) => log::error!("failed to fetch os comments: {}", e),
     }
